0cf1f035501e694659132d98539043a45083a020
Alrenado <eugene1998@ukr.net>
2024-09-17 21:15:43 +0300

    work

--- js/form.js
+++ js/form.js
@@ -53,10 +53,6 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(form);
 
-    // if (userEmailField?.value?.length > 30) {
-    //     return;
-    // }
-
     console.log('Імʼя користувача: ', userNameField.value);
     console.log('Email користувача: ', userEmailField.value);
 
@@ -70,6 +66,5 @@ form.addEventListener('submit', e => {
         launchBtn.setAttribute('enable', true);
         launchBtn.style.opacity = '0';
         clearFormFields();
-        location.reload();
     }, 4000)
 })
